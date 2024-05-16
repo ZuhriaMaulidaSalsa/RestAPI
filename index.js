624b57ade7b8e8ca8d1978a46e27a7e9e8b0b54c
zuhriasalsa <salsazuhria@gmail.com>
2024-05-16 23:40:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const db = require('./connection')
+
+// route utama methods GET
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Akses data nutrisi <a href="/food">di sini</a>.')
+})
+
+app.get('/food', (req, res) => {
+    db.query("SELECT * FROM food", (error, result) =>{
+        // hasil data dari sql
+        console.log(result)
+        res.send(result)
+    })
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
