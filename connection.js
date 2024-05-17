5459a958327767cd54e0444e449b83cb93caf07d
zuhriasalsa <salsazuhria@gmail.com>
2024-05-17 14:30:04 +0700

    p

--- connection.js
+++ connection.js
@@ -1,29 +1,10 @@
-// const mysql = require('mysql')
-
-// const db = mysql.createConnection({
-//     host: "127.0.0.1", 
-//     user: "root", 
-//     password: "", 
-//     database: "app"
-// })
-
-// module.exports = db
-
-const mysql = require('mysql');
+const mysql = require('mysql')
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST, // Gunakan environment variables untuk konfigurasi
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+    host: "127.0.0.1", 
+    user: "root", 
+    password: "", 
+    database: "app"
+})
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database.');
-});
-
-module.exports = db;
+module.exports = db
