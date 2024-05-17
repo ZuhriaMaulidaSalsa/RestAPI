d53ad217a14b5ae028cb47b0423c40b00d27ce52
zuhriasalsa <salsazuhria@gmail.com>
2024-05-17 12:29:55 +0700

    P

--- connection.js
+++ connection.js
@@ -1,10 +1,29 @@
-const mysql = require('mysql')
+// const mysql = require('mysql')
+
+// const db = mysql.createConnection({
+//     host: "127.0.0.1", 
+//     user: "root", 
+//     password: "", 
+//     database: "app"
+// })
+
+// module.exports = db
+
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: "127.0.0.1", 
-    user: "root", 
-    password: "", 
-    database: "app"
-})
+  host: process.env.DB_HOST, // Gunakan environment variables untuk konfigurasi
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
-module.exports = db
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
-const db = require('./connection')
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// const bodyParser = require('body-parser')
+// const db = require('./connection')
 
-// route utama methods GET
-app.use(bodyParser.json())
+// // route utama methods GET
+// app.use(bodyParser.json())
+
+// app.get('/', (req, res) => {
+//     db.query("SELECT * FROM food", (error, result) =>{
+//         // Mengirimkan data sebagai JSON
+//         console.log(result)
+//         res.json(result);
+//     })
+// })
+
+// app.get('/food', (req, res) => {
+//     // db.query("SELECT * FROM food", (error, result) =>{
+//     //     // Mengirimkan data sebagai JSON
+//     //     res.json(result);
+//     // })
+// })
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const db = require('./connection');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    db.query("SELECT * FROM food", (error, result) =>{
-        // Mengirimkan data sebagai JSON
-        console.log(result)
-        res.json(result);
-    })
-})
+  res.send('Akses data nutrisi <a href="/food">di sini</a>.');
+});
 
 app.get('/food', (req, res) => {
-    // db.query("SELECT * FROM food", (error, result) =>{
-    //     // Mengirimkan data sebagai JSON
-    //     res.json(result);
-    // })
-})
-
+  db.query('SELECT * FROM food', (error, result) => {
+    if (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Error retrieving data from database', message: error.message });
+      return;
+    }
+    res.json(result);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
